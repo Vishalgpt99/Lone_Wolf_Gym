10f6947fc5c2ca659755b7fdbd069970fd26a49f
vishalgpt99 <vishalgpt.1999>
2022-07-26 20:20:06 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Logo from '../assets/images/Logo.png';
+
+const Footer = () => (
+  <Box mt="80px" bgColor="#fff3f4">
+    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="Wrap" px="40px" pt="24px">
+      <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} />
+    </Stack>
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Created by VISHAL GUPTA :) </Typography>
+
+  </Box>
+);
+
+export default Footer;
